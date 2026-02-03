12b88d0df99a4809175f60cf68367405a03cb9d7
eng.elsayedebaid <engelsayedebaid@gmail.com>
2026-02-03 16:57:18 +0200

    Create src/App.vue as the root Vue component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Hero />
+    <Projects />
+    <Contact />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import Projects from './components/Projects.vue';
+import Contact from './components/Contact.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Hero,
+    Projects,
+    Contact
+  }
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
